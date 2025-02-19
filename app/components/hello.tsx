5991f348164cf2ccb8f70cac6914c54851b0d0cd
ryanmmbale <mmbaleryan@gmail.com>
2025-02-19 10:07:51 +0300

    routing

---
+++ app/components/hello.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function hello() {
+    console.log("Hello from client components");
+    return (
+        <div>
+            Client component
+        </div>
+    );
+}
+
+export default hello;
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>Dashboard Users</h1>
+
+      <ul className='mt-10'>
+        <li><link href="dashbord/user/1" >User 1</link></li>
+        <li><link href="dashbord/user/2" >User 2</link></li>
+        <li><link href="dashbord/user/3" >User 3</link></li>
+        <li><link href="dashbord/user/4" >User 4</link></li>
+        
+      </ul>
+
+
+    </div>
+  )
+}
+
+export default page
